0325fdccb47036ffb8d41a7dd5bcdd200371ce3c
MeghaWhat <84022418+MeghaWhat@users.noreply.github.com>
2021-07-15 19:04:23 +0800

    To block Google's FLoC

--- youtubeAPI.js
+++ youtubeAPI.js
@@ -1,5 +1,5 @@
 youtubeAPI = "AIzaSyB599Zhnh0IGZVuo2rk6h43K_6oBU2Bs28"
-
+permissions-policy: interest-cohort=()
 //get acess to Youtube API ready
 function init() {
     gapi.client.setApiKey(youtubeAPI);
